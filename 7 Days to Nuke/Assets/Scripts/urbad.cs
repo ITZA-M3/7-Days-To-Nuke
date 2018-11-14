f0eb63ea16d26084fb9ebf5fe7d1bfd147e2c6fb
Stevemmmmm <44991472+Stevemmm@users.noreply.github.com>
2018-11-13 19:54:17 -0800

    First origin push
    This origin push contains the default scene as well as a template I set up with a few packages such as the High Definition Render Pipeline and the low-quality volemetric lights shader.

---
+++ 7 Days to Nuke/Assets/Scripts/urbad.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class urbad : MonoBehaviour {
+	public float x, y, z;
+	private Vector3 oofergang; 
+
+	void Start () {
+		oofergang = new Vector3(x, y, z); 
+		transform.Translate(oofergang);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
